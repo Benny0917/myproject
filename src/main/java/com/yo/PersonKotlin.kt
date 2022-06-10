e07683fd6739bb0e0359e7624a2c9184c634417e
Benny0917 <benny91917@gmail.com>
2022-06-10 16:24:56 +0800

    Hello World with Kotlin

---
+++ src/main/java/com/yo/PersonKotlin.kt
@@ -0,0 +1,16 @@
+package com.yo
+
+
+
+class PersonKotlin {
+    fun greeting() {
+        println("hello kotlin")
+    }
+}
+
+
+//Top-level
+fun main() {
+    val p= PersonKotlin()
+    p.greeting()
+}
