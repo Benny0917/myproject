2cf11ae9abd4f23698cea019e18feca3efd3a26c
Benny0917 <benny91917@gmail.com>
2022-06-10 15:03:44 +0800

    Hello World with class Person

--- src/main/java/com/yo/Hello.java
+++ src/main/java/com/yo/Hello.java
@@ -2,6 +2,8 @@ package com.yo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //System.out.println("Hello World");
+        Person p = new Person();
+        p.greeting();
     }
 }
---
+++ src/main/java/com/yo/Person.java
@@ -0,0 +1,7 @@
+package com.yo;
+
+public class Person {
+    public void greeting() {
+        System.out.println("Hello");
+    }
+}
